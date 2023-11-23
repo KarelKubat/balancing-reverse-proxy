4da0d5727fdfb9b7e80d541a2611908910d6f3f7
Karel Kubat <karel@kubat.nl>
2023-11-23 13:51:32 +0100

    have dummy webserver not send HTTP 500

--- dummy-http-server/multi-test.sh
+++ dummy-http-server/multi-test.sh
@@ -11,15 +11,15 @@ cd $(git rev-parse --show-toplevel) || exit 1
 
 # Start 3 dummy servers.
 go run -- dummy-http-server/main.go -address localhost:8000 -stop-after "$duration" \
-  -delay-responding=false &
+  -delay-responding=false --random-errors=false &
 go run -- dummy-http-server/main.go -address localhost:8001 -stop-after "$duration" \
-  -delay-responding=false &
+  -delay-responding=false  --random-errors=false &
 go run -- dummy-http-server/main.go -address localhost:8002 -stop-after "$duration" \
-  -delay-responding=false &
+  -delay-responding=false  --random-errors=false &
 
 # Start the balancer to use the above dummies as endpoints.
 go run -- balancing-reverse-proxy.go -f \
-  -e http://localhost:8000/first,http://localhost:8001/second,http://localhost:8002/third \
+  -e http://localhost:8000,http://localhost:8001,http://localhost:8002 \
   -s "$duration" \
   -log-file "$logfile" &
 echo "NOTE: The balancer log is $logfile"
